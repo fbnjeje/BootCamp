475de18b466840fa8a7a572387822da629af0a54
fbnjeje <fabiangomes2015@gmail.com>
2023-12-07 12:34:43 -0500

    operador spreed

---
+++ js/Spreed.js
@@ -0,0 +1,7 @@
+function operadorSpread(){
+    let frutas = ['fresa', 'guayaba', 'manzana'];
+    let verduras = ['pepino', 'lechuga', 'espinaca'];
+  
+    let frutasYVerduras = [...frutas, ...verduras];
+  return frutasYVerduras;
+}
