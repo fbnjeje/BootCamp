16fda1855678a560c5b12423d4bdaa3382e0af17
fbnjeje <fabiangomes2015@gmail.com>
2023-12-07 12:19:24 -0500

    condiciones vergas

---
+++ js/condicionesVergas.js
@@ -0,0 +1,14 @@
+
+
+function principal(){
+    
+    var clavos = true;
+    var madera = true;
+    
+    
+    console.log(clavos && madera ? 'puedo hacer una silla':'no puedo hacer una silla')
+    
+    // devuelve true
+}
+
+principal();
