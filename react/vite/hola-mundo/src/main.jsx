1f2373941918b931a729be0ce306f747ba78eaab
fbnjeje <fabiangomes2015@gmail.com>
2023-12-15 23:26:57 -0500

    hello world

--- react/vite/hola-mundo/src/main.jsx
+++ react/vite/hola-mundo/src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.Fragment>
+        <button>hola buton</button>
+        <button>hola buton</button>
+    </React.Fragment>
+
+);
